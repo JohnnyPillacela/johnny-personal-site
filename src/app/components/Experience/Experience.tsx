3d9af51481c39734ff23f37a25c3862374ef3b29
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-04-15 21:22:43 -0400

    add experience type

--- src/app/components/Experience/Experience.tsx
+++ src/app/components/Experience/Experience.tsx
@@ -3,40 +3,13 @@
 import styles from "./Experience.module.css";
 import Image from "next/image";
 
-const WORK_EXPERIENCE = [
-  {
-    id: 1,
-    title: "Software Engineer Intern",
-    company: "Google - Google Payments",
-    start: "May 2021",
-    end: "Aug 2021",
-    logo: "/gPay logo.png"
-  },
-  {
-    id: 2,
-    title: "Software Development Engineer Intern",
-    company: "Amazon - AWS Document DB",
-    start: "June 2022",
-    end: "August 2022",
-    logo: "/docdb_logo.jpg"
-  },
-  {
-    id: 3,
-    title: "Software Development Engineer",
-    company: "Amazon - Prime Video",
-    start: "Nov 2023",
-    end: "Present",
-    logo: "/pv_logo.png"
-  },
-];
-
 export default function Experience() {
   return (
     <section className={styles.section}>
       <h3 className={styles.heading}>Work Experience</h3>
       <div className={styles.jobs}>
         {WORK_EXPERIENCE.map((job) => (
-            <div key={job.id} className={styles.card}>
+          <div key={job.id} className={styles.card}>
             <div className={styles.cardContent}>
               <div className={styles.header}>
                 <Image
@@ -51,11 +24,49 @@ export default function Experience() {
                   <p className={styles.company}>{job.company}</p>
                 </div>
               </div>
-              <p className={styles.dates}>{job.start} – {job.end}</p>
+              <p className={styles.dates}>
+                {job.start} – {job.end}
+              </p>
             </div>
-          </div>          
+          </div>
         ))}
       </div>
     </section>
   );
 }
+
+const WORK_EXPERIENCE: Experience[] = [
+  {
+    id: 1,
+    title: "Software Engineer Intern",
+    company: "Google - Google Payments",
+    start: "May 2021",
+    end: "Aug 2021",
+    logo: "/gPay logo.png",
+  },
+  {
+    id: 2,
+    title: "Software Development Engineer Intern",
+    company: "Amazon - AWS Document DB",
+    start: "June 2022",
+    end: "August 2022",
+    logo: "/docdb_logo.jpg",
+  },
+  {
+    id: 3,
+    title: "Software Development Engineer",
+    company: "Amazon - Prime Video",
+    start: "Nov 2023",
+    end: "Present",
+    logo: "/pv_logo.png",
+  },
+];
+
+type Experience = {
+  id: number;
+  title: string;
+  company: string;
+  start: string;
+  end: string;
+  logo: string;
+};
